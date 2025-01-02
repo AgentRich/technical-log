4fe4bfcac663e133f6d6cee0e9871d82b35655d3
AgentRich <462137025@qq.com>
2025-01-02 20:52:39 +0800

    test update

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -9,36 +9,36 @@ import {
 } from "./chunk-L2JNJ22P.js";
 
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
 // node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
-import "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import { default as default3 } from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
-import { default as default4 } from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
-import { default as default5 } from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
-import { default as default6 } from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default7 } from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default8 } from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default9 } from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPLink.vue";
-import { default as default10 } from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default11 } from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPSocialLink.vue";
-import { default as default12 } from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPSocialLinks.vue";
-import { default as default13 } from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
-import { default as default14 } from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default15 } from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default16 } from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default17 } from "D:/codespace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
+import "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import { default as default3 } from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
+import { default as default4 } from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
+import { default as default5 } from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
+import { default as default6 } from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default7 } from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default8 } from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default9 } from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPLink.vue";
+import { default as default10 } from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default11 } from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPSocialLink.vue";
+import { default as default12 } from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPSocialLinks.vue";
+import { default as default13 } from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
+import { default as default14 } from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default15 } from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default16 } from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default17 } from "C:/codeSpace/technical-log/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 
 // node_modules/vitepress/dist/client/theme-default/support/utils.js
 import { withBase } from "vitepress";
--- docs/technical-log/1/TheInternet.md
+++ docs/technical-log/1/TheInternet.md
@@ -3,6 +3,8 @@
 Coming Soon~!
 :::
 
+[你管这破玩应叫网络？@飞天闪客](https://www.douyin.com/video/7453855860079578395)
+
 [网络中国——第一集 科技的引领](https://v.kepu.net.cn/df/202207/t20220711_498802.html)
 
 [网络中国：第二集 创新的力量](https://v.kepu.net.cn/df/202207/t20220711_498820.html)
